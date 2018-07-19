a1aed8e130b278e03dceabadd1fe962dbfac3a7f
liangpengpeng <liangpengpeng@eshippinggateway.com>
2018-07-19 15:57:48 +0800

    完整框架

--- assets/urls.py
+++ assets/urls.py
@@ -5,4 +5,8 @@ app_name = 'assets'
 
 urlpatterns = [
     url(r'^report/',views.report,name='report'),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
+    url(r'^index/', views.index, name='index'),
+    url(r'^detail/(?P<asset_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^$', views.dashboard),
 ]
